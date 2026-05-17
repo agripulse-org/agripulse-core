354efae9b8024eb2e9b1105dba85ecce25468e22
davidanastasov <davidanastasov@hotmail.com>
2026-05-18 00:58:29 +0200

    KAN-87: Add loading indicator while waiting for SSO to finish

--- frontend/src/routes/auth/sso-callback.tsx
+++ frontend/src/routes/auth/sso-callback.tsx
@@ -1,6 +1,16 @@
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import { createFileRoute } from "@tanstack/react-router";
+import { PageLoader } from "@/components/layout/PageLoader";
 
 export const Route = createFileRoute("/auth/sso-callback")({
-  component: AuthenticateWithRedirectCallback,
+  component: SsoCallback,
 });
+
+function SsoCallback() {
+  return (
+    <>
+      <PageLoader className="min-h-50" />
+      <AuthenticateWithRedirectCallback />
+    </>
+  );
+}
